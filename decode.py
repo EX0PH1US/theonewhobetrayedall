40511e620cd37457d9611b06a8093221e63b4d31
EX0PH1US <162728923+EX0PH1US@users.noreply.github.com>
2025-09-01 23:11:44 +0530

    Update decode.py

--- decode.py
+++ decode.py
@@ -4,6 +4,7 @@ img = Image.open('561.png').convert('L') #8-bit grayscale
 
 raw = img.tobytes() # convert into bytes
 
-n = 561
+n = 561 # length of the text message (even if you don't put it you will still be able to print the encoded text)
+
 
 print(raw[:n].decode('utf-8')) # convert into utf-8
